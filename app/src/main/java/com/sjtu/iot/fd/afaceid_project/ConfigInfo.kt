74187b8f16153578f32374ece107a3ebcd763300
dingfeng <1041460265@qq.com>
2018-11-24 19:21:26 +0800

    修改采样率为48000

--- app/src/main/java/com/sjtu/iot/fd/afaceid_project/ConfigInfo.kt
+++ app/src/main/java/com/sjtu/iot/fd/afaceid_project/ConfigInfo.kt
@@ -12,7 +12,7 @@ class ConfigInfo {
 
     companion object {
         const val audioSource: Int = MediaRecorder.AudioSource.MIC
-        const val sampleRateInHz: Int = 44100
+        const val sampleRateInHz: Int = 48000
         const val channelConfig = AudioFormat.CHANNEL_IN_MONO
         const val audioFormat = AudioFormat.ENCODING_PCM_16BIT
          var bufferSize: Int = 48000*2*2
